fb06e23f36c02f437ee093fd8c6592dad6fb3f6d
Marina <margolnz@gmail.com>
2021-10-02 15:07:46 +1000

    Add cookies and Sessions using Passport.js

--- app.js
+++ app.js
@@ -2,13 +2,24 @@ require('dotenv').config()
 const express = require('express')
 const ejs = require('ejs')
 const mongoose  = require('mongoose')
-const encrypt = require('mongoose-encryption')
+// const encrypt = require('mongoose-encryption')
+const session = require('express-session')
+const passport = require('passport')
+const passportLocalMongoose = require('passport-local-mongoose')
 
 const app = express()
 
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended: true}))
+
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 
 mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true})
 
@@ -17,9 +28,15 @@ const userSchema = new mongoose.Schema ({
     password: String
 })
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']})
+userSchema.plugin(passportLocalMongoose)
+
+// userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']})
 
 const User = new mongoose.model('User', userSchema)
+
+passport.use(User.createStrategy())
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
 
 app.get('/', (req, res)=> {
     res.render('home')
@@ -33,32 +50,43 @@ app.get('/login', (req, res)=> {
     res.render('login')
 })
 
+app.get('/secrets', (req, res)=> {
+    if(req.isAuthenticated()){
+        res.render('secrets')
+    }else{
+        res.redirect('/login')
+    }
+})
+
+app.get('/logout', (req, res) => {
+    req.logout()
+    res.redirect('/')
+})
+
 app.post('/register', (req, res)=> {
-        const newUser = new User ({
-        email: req.body.username,
-        password: req.body.password
-    })
-    newUser.save(err=>{
-        if(err) {
+    User.register({username: req.body.username}, req.body.password, (err, user)=> {
+        if(err){
             console.log(err)
         }else{
-            res.render('secrets')
+            passport.authenticate('local')(req, res, ()=> {
+                res.redirect('/secrets')
+            })
         }
     })
 })
 
 app.post('/login', (req, res)=> {
-    const username = req.body.username
-    const password = req.body.password
-    User.findOne({email: username}, (err, foundUser)=> {
+    const user = new User({
+        username: req.body.username,
+        passsword: req.body.password
+    })
+    req.login(user, (err)=> {
         if(err){
             console.log(err)
         }else{
-            if(foundUser) {
-                if(foundUser.password === password) {
-                    res.render('secrets')
-                }
-            }
+            passport.authenticate('local') (req, res, ()=>{
+                res.redirect('/secrets')
+            })
         }
     })
 })
